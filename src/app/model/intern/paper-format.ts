59794fd7a756cec94eb64f4a502649c20d84baa9
OdinOxin <OdinOxin@googlemail.com>
2025-07-26 14:45:44 +0200

    add paper formats DIN A6 and A2..A0

--- src/app/model/intern/paper-format.ts
+++ src/app/model/intern/paper-format.ts
@@ -1,9 +1,13 @@
 import {PaperOrientation} from "./paper-orientation";
 
 export enum PaperFormat {
+    A0 = "A0",
+    A1 = "A1",
+    A2 = "A2",
     A3 = "A3",
     A4 = "A4",
     A5 = "A5",
+    A6 = "A6",
     CUSTOM = "Custom"
 }
 
@@ -37,9 +41,13 @@ class PaperFormatPropertiesImpl implements PaperFormatProperties {
 }
 
 export const PAPER_FORMATS = new Map<PaperFormat, PaperFormatPropertiesImpl>([
+    [PaperFormat.A6, new PaperFormatPropertiesImpl("A6", 105, 148)],
     [PaperFormat.A5, new PaperFormatPropertiesImpl("A5", 148, 210)],
     [PaperFormat.A4, new PaperFormatPropertiesImpl("A4", 210, 297)],
     [PaperFormat.A3, new PaperFormatPropertiesImpl("A3", 297, 420)],
+    [PaperFormat.A2, new PaperFormatPropertiesImpl("A2", 420, 594)],
+    [PaperFormat.A1, new PaperFormatPropertiesImpl("A1", 594, 841)],
+    [PaperFormat.A0, new PaperFormatPropertiesImpl("A0", 841, 1189)],
     [PaperFormat.CUSTOM, new PaperFormatPropertiesImpl($localize`Custom`, undefined, undefined)]
 ]);
 
